d89cd9e9dca7c48f0ab4d9eb17120db5a5e224de
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-19 08:26:26 -0700

    Create Display value in corresponding width.java

---
+++ Display value in corresponding width.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan=new Scanner(System.in);
+        int a,b;
+        a=scan.nextInt();
+        b=scan.nextInt();
+        System.out.printf("%05d\n",a);
+        System.out.printf("%5d",b);
+    }
+}
