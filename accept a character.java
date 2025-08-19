a29bd5ddf272c359aa8c92335dbde688ee95f866
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-19 08:22:53 -0700

    Create accept a character.java

---
+++ accept a character.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        char s;
+        s=scan.next().charAt(0);
+        System.out.print(s);
+    
+    }
+}
