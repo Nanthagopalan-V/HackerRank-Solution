0666081d8953fa082a7246192f02ab64f4238ae9
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-28 07:23:03 -0700

    Create check the values are equal.java

---
+++ check the values are equal.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a,b;
+        a=scan.nextInt();
+        b=scan.nextInt();
+        if(a==b){
+            System.out.print("EQUAL");
+        }        
+        else{
+            System.out.print("NOT EQUAL");
+        }
+    }
+}
