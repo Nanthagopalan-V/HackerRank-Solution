2a8459e9851a5e484adc0cf828758b069b03e53d
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-28 09:54:07 -0700

    Create number is either divisible by 3 or divisible by 5.java

---
+++ number is either divisible by 3 or divisible by 5.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n;
+        n=scan.nextInt();
+        if(n%3==0){
+            System.out.print("Hi");
+        }
+        if(n%5==0){
+            System.out.print("Hello");
+        }
+    }
+}
