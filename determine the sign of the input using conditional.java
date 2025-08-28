9edb1cd2962f6f0c0d710c6cd408b0ca6e1ff3d1
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-28 07:25:52 -0700

    Create determine the sign of the input using conditional.java

---
+++ determine the sign of the input using conditional.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int a;
+        a=scan.nextInt();
+        if(a>0){
+            System.out.print("Positive");
+        }
+        else if(a==0){
+            System.out.print("Zero");
+        }
+        else{
+            System.out.print("Negative");
+        }
+    }
+}
