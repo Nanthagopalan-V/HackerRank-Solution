68cf00a2b96f55aa50c4b56cc87e2a497cba1eee
Nanthagopalan-V <nanthagopalan155@gmail.com>
2025-08-29 08:30:07 -0700

    Print all the alphabets for given n times.java

---
+++ Print all the alphabets for given n times.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println(); 
+        }
+
+        sc.close();
+    }
+}
